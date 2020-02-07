9a3b6917eab7263819c4e777c1150d899ade1bf0
Gal Kol <999galk@gmail.com>
2020-02-07 18:37:08 +0200

    fixed image box in mobile

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,16 +3,16 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return(
-		<div>
+		<div className='pb4'>
 			<p className='f3'>
 				{'This magic brain will detect faces in your pictures!'}
 			</p>
 			<p className='f3'>
 				{'Submit an image URL and give it a try!'}
 			</p>
-			<div className="form center pa4 w-60 br3 shadow-5">
-				<input className="f4 pa2 w-70 center" type='tex' onChange={onInputChange}/>
-				<button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonSubmit}>Detect!</button>
+			<div className="form center pa4 br3 shadow-5" style={{display:'flex', justifyContent:'center', flexWrap:'wrap'}}>
+				<input className="f4 pa1 min70 mt2" type='tex' placeholder='Image link' onChange={onInputChange}/>
+				<button className=" grow f4 link ph3 pv2 dib white bg-light-purple mt2" onClick={onButtonSubmit}>Detect!</button>
 			</div>
 		</div>
 	);
